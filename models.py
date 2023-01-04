ca4a7289318b21ac8c75d07d532ed3cbd9428ccb
linkolnakakvant <karpovich.valera@mail.ru>
2023-01-04 22:05:05 +0300

    fixx

--- models.py
+++ models.py
@@ -8,11 +8,11 @@ class RequestSchema(Schema):
     cmd = fields.Str(required=True)
     value = fields.Str()
 
-
     @validates_schema
     def check_all_cmd_valid(self, values: dict[str, str], *args, **kwargs):
         if values['cmd'] not in VALID_CMD_COMMANDS:
             raise ValidationError('"cmd" contains invalid value')
+
 
 class BatchRequestSchema(Schema):
     queries = fields.Nested(RequestSchema, many=True)
--- views.py
+++ views.py
@@ -7,12 +7,13 @@ main_bp = Blueprint('main', __name__)
 
 FILE_NAME: str = 'data/apache_logs.txt'
 
+
 @main_bp.route('/perform_query', methods=['POST'])
 def perform_query():
     data = request.json
     validation_data = BatchRequestSchema().load(data)
 
-    result =  None
+    result = None
     for query in validation_data['queries']:
         result = builder_query(
             cmd=query['cmd'],
@@ -20,6 +21,5 @@ def perform_query():
             file_name=FILE_NAME,
             data=result,
         )
-
 
     return jsonify(path.basename(FILE_NAME), result)
